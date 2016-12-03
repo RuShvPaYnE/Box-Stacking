1b1facdcf6a7d4dca20971ad4ad180b63482480e
dowlinga <dowlinga@mail.gvsu.edu>
2016-12-03 14:30:32 -0500

    Update RoseDowlingAssignment4.cpp

--- RoseDowlingAssignment4.cpp
+++ RoseDowlingAssignment4.cpp
@@ -7,12 +7,12 @@ class Boxes{
 	
 	public:
 		void set_values(int,int);
-}
+};
 void Boxes::set_values(int w, int s){
 	weight = w;
 	strength = s-w;
 	
-}
+};
 
 int main(){
 	std::vector<Boxes> boxes;
