364e51905afb3e1b6eadd917cedbe64947d1f526
RuShvPaYnE <roselan@mail.gvsu.edu>
2016-12-03 13:56:57 -0500

    Create RoseDowlingAssignment4.cpp

---
+++ RoseDowlingAssignment4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class Boxes{
+	int weight, strength;
+	
+	public:
+		void set_values(int,int);
+}
+void Boxes::set_values(int w, int s){
+	weight = w;
+	strength = s-w;
+	
+}
+
+int main(){
+	
+	
+	 
+	
+	
+	
+}
